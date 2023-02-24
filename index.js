3c15e0e9876d0ef933fdfde9dd31b7c5e699b79d
kamal98ETH <bestkacm@gmail.com>
2023-02-24 01:25:21 +0100

    feat: about me section

---
+++ index.js
@@ -0,0 +1,24 @@
+// Get the scroll box element
+const scrollBox = document.getElementById('projects');
+console.log(scrollBox)
+
+// Detect when the user scrolls
+window.addEventListener('scroll', function () {
+    // Get the current scroll position
+    const scrollPosition = window.scrollY;
+
+    // Get the height of the viewport
+    const viewportHeight = window.innerHeight;
+
+    // Get the height of the scroll box element
+    const scrollBoxHeight = scrollBox.offsetHeight;
+
+    // Calculate the position at which the scroll box should appear
+    const scrollBoxPosition = (scrollBoxHeight + scrollBox.offsetTop) - viewportHeight;
+
+    // Check if the user has scrolled to the position where the scroll box should appear
+    if (scrollPosition >= scrollBoxPosition) {
+        // Show the scroll box
+        scrollBox.style.opacity = 1;
+    }
+});
