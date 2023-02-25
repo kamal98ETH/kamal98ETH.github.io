d0c76883e00279fe0829faca29933550f06977de
kamal98ETH <bestkacm@gmail.com>
2023-02-25 01:12:35 +0100

    feat: projects section

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-// Get the scroll box element
-const scrollBox = document.getElementById('projects');
-console.log(scrollBox)
+// // Disable scrolling on page load
+// document.body.style.overflow = 'hidden';
 
-// Detect when the user scrolls
-window.addEventListener('scroll', function () {
-    // Get the current scroll position
-    const scrollPosition = window.scrollY;
+// // Wait for 10 seconds before enabling scrolling
+// setTimeout(function () {
+//     document.body.style.overflow = 'auto';
+// }, 8000);
 
-    // Get the height of the viewport
-    const viewportHeight = window.innerHeight;
+// // Get the scroll box element
+// const scrollBox = document.getElementById('projects');
+// console.log(scrollBox)
 
-    // Get the height of the scroll box element
-    const scrollBoxHeight = scrollBox.offsetHeight;
+// // Detect when the user scrolls
+// window.addEventListener('scroll', function () {
+//     // Get the current scroll position
+//     const scrollPosition = window.scrollY;
 
-    // Calculate the position at which the scroll box should appear
-    const scrollBoxPosition = (scrollBoxHeight + scrollBox.offsetTop) - viewportHeight;
+//     // Get the height of the viewport
+//     const viewportHeight = window.innerHeight;
 
-    // Check if the user has scrolled to the position where the scroll box should appear
-    if (scrollPosition >= scrollBoxPosition) {
-        // Show the scroll box
-        scrollBox.style.opacity = 1;
-    }
-});
+//     // Get the height of the scroll box element
+//     const scrollBoxHeight = scrollBox.offsetHeight;
+
+//     // Calculate the position at which the scroll box should appear
+//     const scrollBoxPosition = (scrollBoxHeight + scrollBox.offsetTop) - viewportHeight;
+
+//     // Check if the user has scrolled to the position where the scroll box should appear
+//     if (scrollPosition >= scrollBoxPosition) {
+//         // Show the scroll box
+//         scrollBox.style.opacity = 1;
+//     }
+// });
